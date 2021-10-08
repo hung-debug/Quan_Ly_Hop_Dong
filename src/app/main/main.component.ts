20bb0c6030fd9f6703142ab0b312c1fce90c8167
DoaiNguyen <doainh@vhc.com.vn>
2021-10-08 15:15:45 +0700

    Reset password in project

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-main',
@@ -7,11 +9,63 @@ import { Router } from '@angular/router';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
+  closeResult:string= '';
+  resetPasswordForm:any = FormGroup;
+  fieldTextTypeOld: boolean = false;
+  fieldTextTypeNew: boolean = false;
+  repeatFieldTextTypeNew: boolean = false;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private modalService: NgbModal, private fb: FormBuilder) {}
+
+  //open popup reset password
+  open(content:any) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  //close popup reset password
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
+
+  //form reset password
+  initResetPasswordForm() {
+    this.resetPasswordForm = this.fb.group({
+      passwordOld: ["", [Validators.required]],
+      passwordNew: ["", Validators.required],
+      confirmPasswordNew: ["", Validators.required]
+    });
+  }
+
+  //hien thi password dang text, dang ma
+  toggleFieldTextTypeOld() {
+    this.fieldTextTypeOld = !this.fieldTextTypeOld;
+  }
+
+  //hien thi password dang text, dang ma
+  toggleFieldTextTypeNew() {
+    this.fieldTextTypeNew = !this.fieldTextTypeNew;
+  }
+
+ //hien thi password dang text, dang ma
+  toggleRepeatFieldTextTypeNew() {
+    this.repeatFieldTextTypeNew = !this.repeatFieldTextTypeNew;
+  }
 
   ngOnInit(): void {
+    this.initResetPasswordForm();
   }
+
+  //click logout
   logout() {
     localStorage.removeItem('currentUser');
     this.router.navigate(['/login']);
