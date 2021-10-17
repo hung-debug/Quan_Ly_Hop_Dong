a3bd01b06871060f47fe35127fda3dc907fe9381
DoaiNguyen <doainh@vhc.com.vn>
2021-10-17 22:36:34 +0700

    edit css login and check forgot password

--- src/app/login/forgot-password/forgot-password.component.ts
+++ src/app/login/forgot-password/forgot-password.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-forgot-password',
@@ -9,7 +10,14 @@ import { Router } from '@angular/router';
 })
 export class ForgotPasswordComponent implements OnInit {
   forgotPasswordForm:any =  FormGroup;
-  constructor(private fb: FormBuilder, private router: Router) { }
+  closeResult:string= '';
+  status:number = 1;
+  notification:string = '';
+  error:boolean = false;
+  errorDetail:string = '';
+  constructor(private fb: FormBuilder,
+              private router: Router,
+              private modalService: NgbModal,) { }
 
   initRegForm() {
     this.forgotPasswordForm = this.fb.group({
@@ -21,7 +29,44 @@ export class ForgotPasswordComponent implements OnInit {
   }
 
   sendForgetPassword() {
-    this.router.navigate(['/reset-password']);
+    let email = this.forgotPasswordForm.value.email;
+    if(email == ''){
+      this.error = true;
+      this.errorDetail = 'Tên email không được để trống!';
+    }else{
+      const regularExpression = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if(regularExpression.test(String(email).toLowerCase()) == false){
+        this.error = true;
+        this.errorDetail = 'Tên email sai định dạng!';
+      }else{
+        this.error = false;
+        if(this.status == 0){
+          this.notification = 'Gửi email thất bại. Vui lòng kiểm tra lại thông tin và thử lại!';
+        }else{
+          this.notification = 'Chúng tôi đã gửi thông tin về địa chỉ email '+ email +'. Vui lòng truy cập email để tiếp tục!';
+        }
+      }
+    }
+  }
+
+  //open popup reset password
+  open(content:any) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  //close popup reset password
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
   }
 
 }
--- src/app/login/reset-password/reset-password.component.ts
+++ src/app/login/reset-password/reset-password.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-reset-password',
@@ -11,7 +12,14 @@ export class ResetPasswordComponent implements OnInit {
   resetPasswordForm:any = FormGroup;
   fieldTextType: boolean = false;
   repeatFieldTextType: boolean = false;
-  constructor(private fb: FormBuilder, private router: Router) { }
+  closeResult:string= '';
+  status:number = 1;
+  notification:string = '';
+  error:boolean = false;
+  errorDetail:string = '';
+  constructor(private fb: FormBuilder,
+              private router: Router,
+              private modalService: NgbModal,) { }
 
   initResetPasswordgForm() {
     this.resetPasswordForm = this.fb.group({
@@ -31,7 +39,54 @@ export class ResetPasswordComponent implements OnInit {
     this.repeatFieldTextType = !this.repeatFieldTextType;
   }
 
-  sendResetPassword() {
-    this.router.navigate(['/main/dashboard']);
+  //open popup reset password
+  open(content:any) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
   }
+
+  //close popup reset password
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
+
+  sendResetPassword() {
+    let password = this.resetPasswordForm.value.password;
+    let confirmpassword = this.resetPasswordForm.value.confirmpassword;
+    if(password == ''){
+      this.error = true;
+      this.errorDetail = 'Mật khẩu mới không được để trống!';
+    }else if(confirmpassword == ''){
+      this.error = true;
+      this.errorDetail = 'Xác nhận mật khẩu mới không được để trống!';
+    }else{
+      if(password != confirmpassword){
+        this.error = true;
+        this.errorDetail = 'Xác nhận mật khẩu mới không khớp!';
+      }else{
+        this.error = false;
+        if(this.status == 0){
+          this.notification = 'Đổi mật khẩu mới thất bại!';
+        }else{
+          this.notification = 'Đổi mật khẩu mới thành công. Vui lòng đăng nhập để tiếp tục!';
+        }
+        console.log(this.status + " " + this.error + " " + this.notification);
+      }
+    }
+  }
+
+  //return login
+  returnLogin() {
+    this.router.navigate(['/login']);
+  }
+
 }
