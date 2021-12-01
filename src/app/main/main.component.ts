5091b5fdd4fd2d9cd1823992073165d91f55bb16
DoaiNguyen <doainh@vhc.com.vn>
2021-12-02 02:59:12 +0700

    infor user sign

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -196,14 +196,7 @@ export class MainComponent implements OnInit {
     }
   }
 
-  getCurrentUser(){
-    this.currentUserForm = this.fb.group({
-      name : JSON.parse(localStorage.getItem('currentUser')||'').customer.name,
-      email : JSON.parse(localStorage.getItem('currentUser')||'').customer.email,
-      phone : JSON.parse(localStorage.getItem('currentUser')||'').customer.phone,
-      status : JSON.parse(localStorage.getItem('currentUser')||'').customer.status,
-      organization_id : JSON.parse(localStorage.getItem('currentUser')||'').customer.organization_id,
-      type_id : JSON.parse(localStorage.getItem('currentUser')||'').customer.type_id,
-    });
+  infoUserDetail(){
+    this.router.navigate(['/main/user']);
   }
 }
--- src/app/main/user/user.component.ts
+++ src/app/main/user/user.component.ts
@@ -8,10 +8,26 @@ import { AppService } from 'src/app/service/app.service';
 })
 export class UserComponent implements OnInit {
 
+  name:any;
+  email:any;
+  phone:any;
+  birthday:any;
+  organization_id:any;
+
+  phoneKpi:any;
+  networkKpi:any;
+
+  nameHsm:any;
+
   constructor(private appService: AppService,) { }
 
   ngOnInit(): void {
-    this.appService.setTitle("Thông tin tài khoản");
+    this.appService.setTitle("user.information");
+
+    this.name = JSON.parse(localStorage.getItem('currentUser')||'').customer.name;
+    this.email = JSON.parse(localStorage.getItem('currentUser')||'').customer.email;
+    this.phone = JSON.parse(localStorage.getItem('currentUser')||'').customer.phone;
+    this.organization_id = JSON.parse(localStorage.getItem('currentUser')||'').customer.organization_id;
   }
 
 }
