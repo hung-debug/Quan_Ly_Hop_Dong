e97f467dcb8441db384c7ffa9baabe42ee1adc31
duongdt <dothanhduonglbt.0710@gmail.com>
2022-01-08 14:07:50 +0700

    edit code

--- src/app/main/contract/contract.module.ts
+++ src/app/main/contract/contract.module.ts
@@ -42,11 +42,6 @@ import {NgxInputSearchModule} from "ngx-input-search";
 import { AddConnectDialogComponent } from './dialog/add-connect-dialog/add-connect-dialog.component';
 import { ShareContractDialogComponent } from './dialog/share-contract-dialog/share-contract-dialog.component';
 import { DeleteContractDialogComponent } from './dialog/delete-contract-dialog/delete-contract-dialog.component';
-import {InforContractCopyComponent} from "./copy-contract/model/infor-contract/infor-contract.component";
-import {SampleContractCopyComponent} from "./copy-contract/model/sample-contract/sample-contract.component";
-import {DetermineSignerCopyComponent} from "./copy-contract/model/determine-signer/determine-signer.component"
-import {ContractHeaderCopyComponent} from "./copy-contract/model/contract-header/contract-header.component";
-import {ConfirmInforContractCopyComponent} from "./copy-contract/model/confirm-infor-contract/confirm-infor-contract.component";
 
 const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
   suppressScrollX: true
@@ -70,26 +65,14 @@ const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
     ContractConnectDialogComponent,
     AddConnectDialogComponent,
     ShareContractDialogComponent,
-    DeleteContractDialogComponent,
-    //
-    InforContractCopyComponent,
-    SampleContractCopyComponent,
-    DetermineSignerCopyComponent,
-    ContractHeaderCopyComponent,
-    ConfirmInforContractCopyComponent
+    DeleteContractDialogComponent
   ],
   exports: [
     ContractHeaderComponent,
     InforContractComponent,
     SampleContractComponent,
     ConfirmInforContractComponent,
-    DetermineSignerComponent,
-    //
-    InforContractCopyComponent,
-    SampleContractCopyComponent,
-    DetermineSignerCopyComponent,
-    ContractHeaderCopyComponent,
-    ConfirmInforContractCopyComponent
+    DetermineSignerComponent
   ],
   imports: [
     CommonModule,
@@ -127,13 +110,7 @@ const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
     InforContractComponent,
     SampleContractComponent,
     DetermineSignerComponent,
-    SignContractComponent,
-    //
-    InforContractCopyComponent,
-    SampleContractCopyComponent,
-    DetermineSignerCopyComponent,
-    ContractHeaderCopyComponent,
-    ConfirmInforContractCopyComponent
+    SignContractComponent
   ],
   providers: [
     {
