3aaf2a87e36576a13eb0e9376c79c4060a5c7f2f
kduocnghi <kduocnghi@gmail.com>
2022-01-19 21:03:30 +0700

    fix bug

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  // apiUrl: 'http://14.160.91.174:1387',
-  apiUrl: 'https://econtract.mobifone.vn/service',
+  apiUrl: 'http://14.160.91.174:1387',
+  // apiUrl: 'https://econtract.mobifone.vn/service',
   //apiUrl: 'http://10.16.150.127:8762',
   url_file_content: "http://118.70.81.203:8530/api/dowload-file?id=BAB61E62-DFE0-4420-A208-386D83ED19E1&view=1",
 };
