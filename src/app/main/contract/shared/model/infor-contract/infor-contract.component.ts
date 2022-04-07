ccffc45fb916a93756cc635f118ec37502070334
duongdthy <duongdthy@gmail.com>
2022-04-07 23:49:48 +0700

    fix bug

--- src/app/main/contract/shared/model/infor-contract/infor-contract.component.ts
+++ src/app/main/contract/shared/model/infor-contract/infor-contract.component.ts
@@ -340,12 +340,11 @@ export class InforContractComponent implements OnInit, AfterViewInit, OnChanges 
           if (this.datas.attachFileArr != null) {
             for (var i = 0; i < this.datas.attachFileArr.length; i++) {
               await this.uploadService.uploadFile(this.datas.attachFileArr[i]).toPromise().then((data) => {
-                this.datas.attachFileArr[i].file_path = data.file_object.file_path;
-                this.datas.attachFileArr[i].name = data.file_object.filename; 
-
-                // this.datas.filePathAttach = data.file_object.file_path;
-                // this.datas.fileNameAttach = data.file_object.filename;
-                // this.datas.fileBucketAttach = data.file_object.bucket;
+                // this.datas.attachFileArr[i].file_path = data.file_object.file_path;
+                // this.datas.attachFileArr[i].name = data.file_object.filename; 
+                this.datas.filePathAttach = data.file_object.file_path;
+                this.datas.fileNameAttach = data.file_object.filename;
+                this.datas.fileBucketAttach = data.file_object.bucket;
                 this.contractService.addDocumentAttach(this.datas).toPromise().then((data) => {
                   this.datas.document_attach_id = data?.id;
                 },
