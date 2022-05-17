a8d804b5776fab1777d71416a166144009cd2467
doainh <doainh@vhc.com.vn>
2022-05-17 11:36:39 +0700

    add pack admin

--- src/app/service/admin/admin-pack.service.ts
+++ src/app/service/admin/admin-pack.service.ts
@@ -9,6 +9,9 @@ export class AdminPackService {
   listPackUrl: any = `${environment.apiUrl}/api/v1/organizations/search`;
   getPackUrl: any = `${environment.apiUrl}/api/v1/organizations/search`;
   deletePackUrl: any = `${environment.apiUrl}/api/v1/organizations/search`;
+  updatePackUrl: any = `${environment.apiUrl}/api/v1/organizations/`;
+  checkNameUniqueUrl:any = `${environment.apiUrl}/api/v1/organizations/check-name-unique`;
+  checkCodeUniqueUrl:any = `${environment.apiUrl}/api/v1/organizations/check-code-unique`;
 
   constructor(
     private http: HttpClient,
@@ -41,5 +44,52 @@ export class AdminPackService {
       .append('Authorization', 'Bearer ' + this.token);
     const body = JSON.stringify({});
     return this.http.post<any>(this.deletePackUrl + id, body, {'headers': headers});
+  }
+
+  updatePack(datas: any) {
+    this.getCurrentUser();
+    const headers = new HttpHeaders()
+      .append('Content-Type', 'application/json')
+      .append('Authorization', 'Bearer ' + this.token);
+    const body = JSON.stringify({
+      name: datas.name,
+      short_name: datas.short_name,
+      code: datas.code,
+      email: datas.email,
+      phone: datas.phone,
+      fax: datas.fax,
+      status: datas.status,
+      parent_id: datas.parent_id,
+      path: datas.path
+    });
+    console.log(headers);
+    console.log(body);
+    return this.http.put<any>(this.updatePackUrl + datas.id, body, {'headers': headers});
+  }
+
+  checkNameUnique(data:any, name:any){
+    this.getCurrentUser();
+    const headers = new HttpHeaders()
+      .append('Content-Type', 'application/json')
+      .append('Authorization', 'Bearer ' + this.token);
+    const body = JSON.stringify({
+      name: name,
+      org_id_cha: data.parent_id,
+      org_id_con: data.id
+    });
+    return this.http.post<any>(this.checkNameUniqueUrl, body, {headers}).pipe();
+  }
+  
+  checkCodeUnique(data:any, code:any){
+    this.getCurrentUser();
+    const headers = new HttpHeaders()
+      .append('Content-Type', 'application/json')
+      .append('Authorization', 'Bearer ' + this.token);
+    const body = JSON.stringify({
+        code: code,
+        org_id_cha: data.parent_id,
+        org_id_con: data.id
+      });
+    return this.http.post<any>(this.checkCodeUniqueUrl, body, {headers}).pipe();
   }
 }
