b3a29b0ba064d4da8ee4ff74b97d14a6445988c2
dung111999 <dung111999@gmail.com>
2022-06-17 14:15:09 +0700

    form chi tiet goi dich vu cua to chuc

--- src/app/admin/admin-main/admin-unit/admin-detail-pack-unit/admin-detail-pack-unit.component.ts
+++ src/app/admin/admin-main/admin-unit/admin-detail-pack-unit/admin-detail-pack-unit.component.ts
@@ -1,23 +1,42 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { AdminUnitService } from 'src/app/service/admin/admin-unit.service';
+import { ToastService } from 'src/app/service/toast.service';
 
 @Component({
   selector: 'app-admin-detail-pack-unit',
   templateUrl: './admin-detail-pack-unit.component.html',
-  styleUrls: ['./admin-detail-pack-unit.component.scss']
+  styleUrls: ['./admin-detail-pack-unit.component.scss'],
 })
 export class AdminDetailPackUnitComponent implements OnInit {
+  datas: any;
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public data: any,
     public dialogRef: MatDialogRef<AdminDetailPackUnitComponent>,
-  ) { }
+    private adminUnitService: AdminUnitService,
+    private toastService: ToastService
+  ) {}
 
   ngOnInit(): void {
+    this.adminUnitService
+      .getPackUnitByIdPack(this.data.id, this.data.idPack)
+      .subscribe(
+        (response) => {
+          console.log("response ", response);
+          this.datas = response;
+        },
+        (error) => {
+          this.toastService.showErrorHTMLWithTimeout(
+            'Có lỗi! Vui lòng liên hệ nhà phát triển để được xử lý',
+            '',
+            3000
+          );
+        }
+      );
   }
 
   cancel() {
     this.dialogRef.close();
   }
-
 }
