a3488877843421908cc854432a085c849e8e167e
dung111999 <dung111999@gmail.com>
2022-10-28 13:57:04 +0700

    comment phần đăng nhập bằng email/số điện thoại phần mẫu hợp đồng do đang test lỗi

--- src/app/main/contract-template/shared/model/determine-signer/determine-signer.component.ts
+++ src/app/main/contract-template/shared/model/determine-signer/determine-signer.component.ts
@@ -95,7 +95,7 @@ export class DetermineSignerComponent implements OnInit {
 
     // data Tổ chức của tôi
     this.data_organization = this.datas.is_determine_clone.filter((p: any) => p.type == 1)[0];
-    this.data_organization.name = this.datas.name_origanzation ? this.datas.name_origanzation : '';
+    this.data_organization.name = this.data_organization.name ? this.data_organization.name : this.datas.name_origanzation;
     this.is_origanzation_reviewer = this.data_organization.recipients.filter((p: any) => p.role == 2);
     this.is_origanzation_signature = this.data_organization.recipients.filter((p: any) => p.role == 3);
     this.is_origanzation_document = this.data_organization.recipients.filter((p: any) => p.role == 4);
