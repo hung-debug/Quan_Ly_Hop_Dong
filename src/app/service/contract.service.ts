8c8ce7912af01f96a10739107966efd21d4d76a5
dung111999 <dung111999@gmail.com>
2022-11-08 06:07:34 +0700

    đổi link api của xác thực ảnh eKYC

--- src/app/service/contract.service.ts
+++ src/app/service/contract.service.ts
@@ -105,9 +105,9 @@ export class ContractService {
 
   getFilePdfForMobileUrl: any = `${environment.apiUrl}/api/v1/contracts/review/`;
 
-  cccdFront: any = `http://ekyc2.mobifone.ai/v2/recognition`;
+  cccdFront: any = `https://mobifone-econtract.vn/eKYC/recognition`;
 
-  detectFaceUrl: any = `http://ekyc2.mobifone.ai/v2/verification`;
+  detectFaceUrl: any = `https://mobifone-econtract.vn/eKYC/verification`;
 
   changeLinkUrl: any = `${environment.apiUrl}/api/v1/handle/`;
 
