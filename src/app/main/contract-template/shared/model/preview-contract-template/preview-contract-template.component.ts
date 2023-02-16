e477973ee8136de1feaf82c24898d3e49fe1e322
dung111999 <dung111999@gmail.com>
2023-02-16 15:59:39 +0700

    fix so hop dong

--- src/app/main/contract-template/shared/model/preview-contract-template/preview-contract-template.component.ts
+++ src/app/main/contract-template/shared/model/preview-contract-template/preview-contract-template.component.ts
@@ -102,9 +102,9 @@ export class PreviewContractTemplateComponent implements OnInit {
     if (d['height'] && (d.sign_unit != 'text' && d.sign_unit != 'so_tai_lieu')) {
       style.height = parseInt(d['height']) + "px";
     }
-    if (this.datas.contract_no && d.sign_unit == 'so_tai_lieu') {
-      style.padding = '6px';
-    }
+    // if (this.datas.contract_no && d.sign_unit == 'so_tai_lieu') {
+    //   style.padding = '6px';
+    // }
     return style;
   }
 
