e56a1a60c92c70aa3ab7715553a6d90ce7a5cce4
dung111999 <dung111999@gmail.com>
2023-03-15 07:11:54 +0700

    process

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -98,11 +98,13 @@ import { AdminInfoUserComponent } from './admin/admin-main/admin-user/admin-info
 
 import {WebcamModule} from 'ngx-webcam';
 import { PdfViewerModule } from 'ng2-pdf-viewer';
-import { ReportStatusContractComponent } from './main/report/report-status-contract/report-status-contract.component';
 import { ReportDetailComponent } from './main/report/report-detail/report-detail.component';
+import { ReportSoonExpireComponent } from './main/report/report-soon-expire/report-soon-expire.component';
+import { ReportStatusContractComponent } from './main/report/report-status-contract/report-status-contract.component';
 import { ReportContractNumberFollowStatusComponent } from './main/report/report-contract-number-follow-status/report-contract-number-follow-status.component';
 import { ContractNumberFollowTypeComponent } from './main/report/contract-number-follow-type/contract-number-follow-type.component';
 import { ContractNumberFollowSignComponent } from './main/report/contract-number-follow-sign/contract-number-follow-sign.component';
+
 
 
 const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
@@ -168,11 +170,13 @@ const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
     AdminDeleteUnitComponent,
     AdminDeletePackUnitComponent,
     AdminInfoUserComponent,
-    ReportStatusContractComponent,
+
     ReportDetailComponent,
+    ReportSoonExpireComponent,
+    ReportStatusContractComponent,
     ReportContractNumberFollowStatusComponent,
     ContractNumberFollowTypeComponent,
-    ContractNumberFollowSignComponent,
+    ContractNumberFollowSignComponent
   ],
   imports: [
     TranslateModule,
