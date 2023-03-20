00de1bc95a012dcf33f3ac7097d885ad751e8574
dung111999 <dung111999@gmail.com>
2023-03-20 14:12:25 +0700

    fix

--- src/app/main/report/report-contract-number-follow-status/report-contract-number-follow-status.component.ts
+++ src/app/main/report/report-contract-number-follow-status/report-contract-number-follow-status.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AppService } from 'src/app/service/app.service';
+import { InputTreeService } from 'src/app/service/input-tree.service';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-report-contract-number-follow-status',
@@ -8,12 +10,137 @@ import { AppService } from 'src/app/service/app.service';
 })
 export class ReportContractNumberFollowStatusComponent implements OnInit {
 
+   //Biến lưu dữ liệu trong bảng
+   list: any[];
+
+   //col header
+   cols: any[];
+ 
+   colsSuggest: any[];
+ 
+   selectedNodeOrganization: any;
+   listOrgCombobox: any[];
+   organization_id: any = '';
+   lang: any;
+   orgListTmp: any[] = [];
+   orgList: any[] = [];
+   organization_id_user_login: any;
+ 
+   //Biến lấy số lượng tổ chức lớn nhất trong các hợp đồng
+   maxOrg: number;
+ 
+   //Biến để gộp các cột
+   mergeCol: any[] = [];
+ 
+   params: any;
+   date: any;
+   optionsStatus: any;
+ 
+   formGroup: any;
+
   constructor(
-    private appService: AppService
+    private appService: AppService,
+    private userService: UserService,
+    private inputTreeService: InputTreeService
   ) { }
 
   ngOnInit(): void {
     this.appService.setTitle('report.number.contracts.status.full');
+
+      //lay id user
+      this.organization_id_user_login =
+      this.userService.getAuthCurrentUser().organizationId;
+    //mac dinh se search theo ma to chuc minh
+    this.organization_id = this.organization_id_user_login;
+
+    //lấy danh sách tổ chức
+    this.inputTreeService.getData().then((res: any) => {
+      this.listOrgCombobox = res;
+
+      this.selectedNodeOrganization = this.listOrgCombobox.filter(
+        (p: any) => p.data == this.organization_id
+      );
+    });
+
+    this.cols = [
+      // {
+      //   id: 1,
+      //   header: 'contract.name',
+      //   style: 'text-align: left;',
+      //   colspan: 1,
+      //   rowspan: '2',
+      // },
+      // {
+      //   id: 2,
+      //   header: 'contract.type',
+      //   style: 'text-align: left;',
+      //   colspan: 1,
+      //   rowspan: '2',
+      // },
+      // {
+      //   id: 3,
+      //   header: 'contract.number',
+      //   style: 'text-align: left;',
+      //   colspan: 1,
+      //   rowspan: '2',
+      // },
+      // {
+      //   id: 4,
+      //   header: 'contract.uid',
+      //   style: 'text-align: left;',
+      //   colspan: 1,
+      //   rowspan: '2',
+      // },
+      // {
+      //   id: 5,
+      //   header: 'contract.connect',
+      //   style: 'text-align: left',
+      //   colspan: 1,
+      //   rowspan: '2',
+      // },
+      // {
+      //   id: 6,
+      //   header: 'contract.time.create',
+      //   style: 'text-align: left',
+      //   colspan: 1,
+      //   rowspan: '2',
+      // },
+      // {
+      //   id: 7,
+      //   header: 'signing.expiration.date',
+      //   style: 'text-align: left',
+      //   colspan: 1,
+      //   rowspan: '2',
+      // },
+      {
+        id: 8,
+        header: 'contract.status.v2',
+        style: 'text-align:left',
+        colspan: 1,
+        rowspan: '2',
+      },
+      // {
+      //   id: 9,
+      //   header: 'date.completed',
+      //   style: 'text-align: left',
+      //   colspan: 1,
+      //   rowspan: '2',
+      // },
+      // {
+      //   id: 10,
+      //   header: 'suggest',
+      //   style: 'text-align: center',
+      //   colspan: '5',
+      //   rowspan: 1,
+      // },
+      {
+        id: 13,
+        header: 'chart.number',
+        style: 'text-align:left',
+        colspan: 1,
+        rowspan: '2',
+      }
+    ];
   }
 
 }
--- src/app/main/report/report-status-contract/report-status-contract.component.ts
+++ src/app/main/report/report-status-contract/report-status-contract.component.ts
@@ -1,47 +1,65 @@
 import { Component, OnInit } from '@angular/core';
 import { AppService } from 'src/app/service/app.service';
+import { InputTreeService } from 'src/app/service/input-tree.service';
+import { UserService } from 'src/app/service/user.service';
 
 @Component({
   selector: 'app-report-status-contract',
   templateUrl: './report-status-contract.component.html',
-  styleUrls: ['./report-status-contract.component.scss']
+  styleUrls: ['./report-status-contract.component.scss'],
 })
 export class ReportStatusContractComponent implements OnInit {
+  //Biến lưu dữ liệu trong bảng
+  list: any[];
 
-    //Biến lưu dữ liệu trong bảng
-    list: any[];
+  //col header
+  cols: any[];
 
-    //col header
-    cols: any[];
-  
-    colsSuggest: any[];
-  
-    selectedNodeOrganization: any;
-    listOrgCombobox: any[];
-    organization_id: any = '';
-    lang: any;
-    orgListTmp: any[] = [];
-    orgList: any[] = [];
-    organization_id_user_login: any;
-  
-    //Biến lấy số lượng tổ chức lớn nhất trong các hợp đồng
-    maxOrg: number;
-  
-    //Biến để gộp các cột
-    mergeCol: any[] = [];
-  
-    params: any;
-    date: any;
-    optionsStatus: any;
-  
-    formGroup: any;
+  colsSuggest: any[];
+
+  selectedNodeOrganization: any;
+  listOrgCombobox: any[];
+  organization_id: any = '';
+  lang: any;
+  orgListTmp: any[] = [];
+  orgList: any[] = [];
+  organization_id_user_login: any;
+
+  //Biến lấy số lượng tổ chức lớn nhất trong các hợp đồng
+  maxOrg: number;
+
+  //Biến để gộp các cột
+  mergeCol: any[] = [];
+
+  params: any;
+  date: any;
+  optionsStatus: any;
+
+  formGroup: any;
 
   constructor(
-    private appService: AppService
-  ) { }
+    private appService: AppService,
+    private userService: UserService,
+    private inputTreeService: InputTreeService
+  ) {}
 
   ngOnInit(): void {
     this.appService.setTitle('report.processing.status.contract.full');
+
+    //lay id user
+    this.organization_id_user_login = this.userService.getAuthCurrentUser().organizationId;
+    
+    //mac dinh se search theo ma to chuc minh
+    this.organization_id = this.organization_id_user_login;
+
+    //lấy danh sách tổ chức
+    this.inputTreeService.getData().then((res: any) => {
+      this.listOrgCombobox = res;
+
+      this.selectedNodeOrganization = this.listOrgCombobox.filter(
+        (p: any) => p.data == this.organization_id
+      );
+    });
 
     this.cols = [
       {
@@ -134,8 +152,7 @@ export class ReportStatusContractComponent implements OnInit {
         style: 'text-align: center',
         colspan: '5',
         rowspan: 1,
-      }
+      },
     ];
   }
-
 }
