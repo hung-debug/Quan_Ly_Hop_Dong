633309d43a384af44682e2a2501ba4dd05036581
dung111999 <dung111999@gmail.com>
2023-04-21 13:39:45 +0700

    fix

--- src/app/service/check-zoom.service.ts
+++ src/app/service/check-zoom.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { ToastService } from './toast.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CheckZoomService {
 
-  constructor() { }
+  constructor(
+    private toastService: ToastService,
+  ) { }
+
+  
+  onResize() {
+    if (window.navigator.userAgent.indexOf('Windows') != -1) {
+      if(window.outerWidth  / window.innerWidth > 1.15 || window.outerWidth  / window.innerWidth < 0.95) {
+        this.toastService.showErrorHTMLWithTimeout('Cảnh báo lệch toạ độ khi kéo ô ký/text/số hợp đồng khi phóng to, thu nhỏ','',3000);
+      } else if(window.devicePixelRatio > 1.15 || window.devicePixelRatio < 0.95) {
+          this.toastService.showErrorHTMLWithTimeout('Cảnh báo lệch toạ độ khi kéo ô ký/text/số hợp đồng khi phóng to, thu nhỏ','',3000);
+      }
+    }
+
+    if (window.navigator.userAgent.indexOf('Mac') != -1) {
+      if(window.outerWidth  / window.innerWidth > 1.15 || window.outerWidth  / window.innerWidth < 0.95) {
+        this.toastService.showErrorHTMLWithTimeout('Cảnh báo lệch toạ độ khi kéo ô ký/text/số hợp đồng khi phóng to, thu nhỏ','',3000);
+      } else if(window.devicePixelRatio != 2) {
+          this.toastService.showErrorHTMLWithTimeout('Cảnh báo lệch toạ độ khi kéo ô ký/text/số hợp đồng khi phóng to, thu nhỏ','',3000);
+      }
+    }
+
+  }
 }
