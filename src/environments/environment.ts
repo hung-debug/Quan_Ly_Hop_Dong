ae3f23eed22ed5eb194b910c28bdf992470231b5
dung111999 <dung111999@gmail.com>
2023-04-30 07:47:05 +0700

    deploy site KD

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,19 +6,19 @@
 export const environment = {
   production: false,
   
-  apiUrl: 'http://14.160.91.174:1387',
+  // apiUrl: 'http://14.160.91.174:1387',
   // apiUrl: 'http://192.168.0.101:8762',
   // apiUrl: 'http://192.168.1.14:8762',
   // flag: 'NB',
-  flag: 'KD',
+  // flag: 'KD',
 
   //NB
   // apiUrl: 'https://econtract.mobifone.vn/service',
   // flag: 'NB',
 
   //KD
-  // apiUrl: 'https://mobifone-econtract.vn/service',
-  // flag: 'KD',
+  apiUrl: 'https://mobifone-econtract.vn/service',
+  flag: 'KD',
   // apiUrl: 'https://mobifone-econtract.vn/service',
   // flag: 'KD',
 
