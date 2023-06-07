dc1f8e9d9756a9c557e753dc6270f832f6dfee61
dung111999 <dung111999@gmail.com>
2023-06-07 10:32:56 +0700

    call api trừ ekyc

--- src/app/main/contract-signature/components/consider-contract/ekyc-dialog-sign/ekyc-dialog-sign.component.ts
+++ src/app/main/contract-signature/components/consider-contract/ekyc-dialog-sign/ekyc-dialog-sign.component.ts
@@ -186,10 +186,14 @@ export class EkycDialogSignComponent implements OnInit {
         //up file anh len db
         this.upFileImageToDb(formData);
   
-        this.contractService.detectFace(this.data.cccdFront, this.webcamImage.imageAsDataUrl).subscribe((response) => {
+        this.contractService.detectFace(this.data.cccdFront, this.webcamImage.imageAsDataUrl).subscribe(async (response) => {
           this.spinner.hide();
           if(response.verify_result == 2 && response.face_anti_spoof_status == 'REAL') {
             alert(this.translate.instant('confirm.success'));
+
+            //call api trừ ekyc
+            await this.contractService.decreaseNumberOfEkyc(this.organizationId).toPromise();
+
             this.dialogRef.close(response.verify_result);
           } else {
             if(response.face_anti_spoof_status == 'FAKE') {
