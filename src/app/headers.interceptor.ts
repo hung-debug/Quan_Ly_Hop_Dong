4b215bd3c082efeb619b89a707e3b2436466a3c6
dung111999 <dung111999@gmail.com>
2023-07-25 08:27:55 +0700

    redirect ra login khi error là 401

---
+++ src/app/headers.interceptor.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpErrorResponse
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
+
+@Injectable()
+export class HeadersInterceptor implements HttpInterceptor {
+
+  constructor(
+    private router: Router,
+  ) {}
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    return next.handle(request).pipe(
+      catchError((error: HttpErrorResponse) => {
+        if (error.status === 401) {
+          this.router.navigate(['/login']);
+        }
+        // Nếu không phải lỗi 401, tiếp tục quăng lỗi để xử lý ở các nơi khác
+        return throwError(error);
+      })
+    );
+  }
+
+}
