c0e267a684453c6178a723ffd8d95f4e94b90c8d
dung111999 <dung111999@gmail.com>
2023-07-25 09:29:03 +0700

    fix xoá loại hợp đồng không chọn lại được option ceca

--- src/app/main/contract-signature/components/consider-contract/image-dialog-sign/image-dialog-sign.component.ts
+++ src/app/main/contract-signature/components/consider-contract/image-dialog-sign/image-dialog-sign.component.ts
@@ -106,13 +106,11 @@ export class ImageDialogSignComponent implements OnInit, AfterViewInit {
   }
 
   handleUpload(event: any) {
-    
     const file = event.target.files[0];
     const reader = new FileReader();
     reader.readAsDataURL(file);
     reader.onload = () => {
       this.imgSignPCSelect = reader.result? reader.result.toString() : '';
-      
     };
   }
 
