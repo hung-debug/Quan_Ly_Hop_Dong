836ee94ccbab2f15e92c760b7c3e6fa00ee91bc5
dung111999 <dung111999@gmail.com>
2023-07-25 08:47:31 +0700

    vẫn để options tải file ảnh chữ ký trên mobile

--- src/app/headers.interceptor.ts
+++ src/app/headers.interceptor.ts
@@ -6,7 +6,7 @@ import {
   HttpInterceptor,
   HttpErrorResponse
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { Router } from '@angular/router';
 
