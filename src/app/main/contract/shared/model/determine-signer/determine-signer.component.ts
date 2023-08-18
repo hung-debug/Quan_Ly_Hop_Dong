20a879c5e6bf0abd9eafbeb55607b39dc6f8c490
mbf/hoangha_develop <ha.danghoang@mobifone.vn>
2023-08-18 12:41:44 +0700

    fix lại điều kiện set disable button Next

--- src/app/main/contract/shared/model/determine-signer/determine-signer.component.ts
+++ src/app/main/contract/shared/model/determine-signer/determine-signer.component.ts
@@ -204,8 +204,6 @@ export class DetermineSignerComponent implements OnInit {
   // next step event
   isCeCaPushNo: boolean = false;
   next(action: string) {
-    this.isButtonDisabled = this.validData() ? true : false;
-
     if (!this.isOrderValueValid) {
       return
     }
@@ -252,6 +250,7 @@ export class DetermineSignerComponent implements OnInit {
       return;
     } else {
       let is_save = false;
+      this.isButtonDisabled = true
       if (action == 'save-step') {
         is_save = true;
 
