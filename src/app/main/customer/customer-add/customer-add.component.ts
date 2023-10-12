d2bc69096bdff0239fe69aa46f11a1079dddf78f
dung111999 <dung111999@gmail.com>
2023-10-12 09:25:57 +0700

    fix change login by email/phone customer module

--- src/app/main/customer/customer-add/customer-add.component.ts
+++ src/app/main/customer/customer-add/customer-add.component.ts
@@ -458,10 +458,20 @@ export class CustomerAddComponent implements OnInit, OnDestroy {
   }
 
   changeTypeSign(d: any,index: any,id?: any,role?: any) {
-    
-    
-    if (d.login_by == 'phone' || d.login_by == 'email') {
+    if(d.login_by == 'phone') {
+      d.signType = d.signType.filter((p: any) => ![2].includes(p.id));
+      d.emailTemp = d.email;
+      d.phone = d.phoneTemp;
+      d.email = d.phone;
+    }
+
+    if(d.login_by == 'email' && d.emailTemp) {
+      d.email = d.emailTemp;
+      d.phoneTemp = d.phone;
+      d.phone = '';
+    } else if(d.login_by == 'email') {
       d.email = '';
+      d.phoneTemp = d.phone;
       d.phone = '';
     }
 
