766da89980e70687da502958d6830a99413e3a97
mbf/hoangha_develop <ha.danghoang@mobifone.vn>
2024-03-06 01:04:09 +0700

    [Hotfix - ConsiderContract] Check null điều kiện phân biệt site NB/KD role văn thư

--- src/app/main/contract-signature/components/consider-contract/consider-contract.component.ts
+++ src/app/main/contract-signature/components/consider-contract/consider-contract.component.ts
@@ -597,7 +597,7 @@ export class ConsiderContractComponent
         this.datas.roleContractReceived = this.recipient.role;
 
         for (const signUpdate of this.isDataObjectSignature) {
-          if (signUpdate && (signUpdate.type == 3 || signUpdate.type == 2 || ((signUpdate.recipient.role == 4 && this.isNB))) &&
+          if (signUpdate && (signUpdate.type == 3 || signUpdate.type == 2 || ((signUpdate?.recipient?.role == 4 && this.isNB))) &&
             [3, 4].includes(this.datas.roleContractReceived) &&
             signUpdate?.recipient?.email === this.currentUser.email &&
             signUpdate?.recipient?.role === this.datas?.roleContractReceived
@@ -2024,7 +2024,7 @@ export class ConsiderContractComponent
                 const textSignB = await domtoimage.toPng(imageRender);
                 signI = this.textSignBase64Gen = textSignB.split(',')[1];
               }
-            } else if (signUpdate.type == 3 || ((signUpdate.recipient.role == 4 && this.isNB) && this.isNB)) {
+            } else if (signUpdate.type == 3 || ((signUpdate?.recipient?.role == 4 && this.isNB))) {
               //lấy ảnh chữ ký usb token
               let imageRender: any = '';
 
@@ -2082,7 +2082,7 @@ export class ConsiderContractComponent
                 }
 
                 if (imageRender) {
-                  if (signUpdate.type == 3 || ((signUpdate.recipient.role == 4 && this.isNB) && this.isNB)) {
+                  if (signUpdate.type == 3 || ((signUpdate?.recipient?.role == 4 && this.isNB))) {
                     signI = this.srcMark.split(',')[1]
                   } else {
                     const textSignB = await domtoimage.toPng(imageRender, this.getOptions(imageRender));
@@ -2268,7 +2268,7 @@ export class ConsiderContractComponent
           const objSign = this.isDataObjectSignature.filter(
             (signUpdate: any) =>
               signUpdate &&
-              (signUpdate.type == 3 || ((signUpdate.recipient.role == 4 && this.isNB) && this.isNB)) &&
+              (signUpdate.type == 3 || ((signUpdate?.recipient?.role == 4 && this.isNB))) &&
               [3, 4].includes(this.datas.roleContractReceived) &&
               signUpdate?.recipient?.email === this.currentUser.email &&
               signUpdate?.recipient?.role === this.datas?.roleContractReceived
@@ -2322,7 +2322,7 @@ export class ConsiderContractComponent
               const textSignB = await domtoimage.toPng(imageRender);
               signI = this.textSignBase64Gen = textSignB.split(',')[1];
             }
-          } else if (signUpdate.type == 3 || ((signUpdate.recipient.role == 4 && this.isNB) && this.isNB)) {
+          } else if (signUpdate.type == 3 || ((signUpdate?.recipient?.role == 4 && this.isNB))) {
 
             try {
               this.isDateTime = await this.timeService.getRealTime().toPromise();
@@ -2491,7 +2491,7 @@ export class ConsiderContractComponent
           const objSign = this.isDataObjectSignature.filter(
             (signUpdate: any) =>
               signUpdate &&
-              (signUpdate.type == 3 || ((signUpdate.recipient.role == 4 && this.isNB) && this.isNB)) &&
+              (signUpdate.type == 3 || ((signUpdate?.recipient?.role == 4 && this.isNB))) &&
               [3, 4].includes(this.datas.roleContractReceived) &&
               signUpdate?.recipient?.email === this.currentUser.email &&
               signUpdate?.recipient?.role === this.datas?.roleContractReceived
@@ -2562,7 +2562,7 @@ export class ConsiderContractComponent
               const textSignB = await domtoimage.toPng(imageRender);
               signI = this.textSignBase64Gen = textSignB.split(',')[1];
             }
-          } else if (signUpdate.type == 3 || ((signUpdate.recipient.role == 4 && this.isNB) && this.isNB)) {
+          } else if (signUpdate.type == 3 || ((signUpdate?.recipient?.role == 4 && this.isNB))) {
             // this.nameCompany = this.recipient.name;
 
             this.widthSign = signUpdate.width;
@@ -3146,7 +3146,7 @@ export class ConsiderContractComponent
     for (const signUpdate of this.isDataObjectSignature) {
       if (
         signUpdate &&
-        (signUpdate.type == 3 || ((signUpdate.recipient.role == 4 && this.isNB) && this.isNB)) &&
+        (signUpdate.type == 3 || ((signUpdate?.recipient?.role == 4 && this.isNB))) &&
         [3, 4].includes(this.datas.roleContractReceived) &&
         signUpdate?.recipient?.email === this.currentUser.email &&
         signUpdate?.recipient?.role === this.datas?.roleContractReceived
