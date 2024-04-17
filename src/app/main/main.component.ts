3d5fadbe322d9a30da2ab62705da2b5b87420317
mbf/hoangha_develop <ha.danghoang@mobifone.vn>
2024-04-17 17:44:52 +0700

    [Fix - MainComponent] Bỏ khai báo biến thừa

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -34,7 +34,6 @@ export class MainComponent implements OnInit {
   nameCurrentUser:any;
   listNotification: any[] = [];
   getAlllistNotification: any[] = [];
-  ssoExpireDate = environment.ssoExpireDate
 
   constructor(private router: Router,
               private appService: AppService,
