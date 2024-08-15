e8eb87b63e019897530e011ed04dfb2efe85bbeb
Nguyen Huu Quyen <quyen.nguyenhuu@mobifone.vn>
2024-08-15 10:42:42 +0000

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
-  usedSSO: true,
+  usedSSO: false,
   apiUrl: 'https://econtractdev.mobifone.ai/service',
   // apiUrl: 'https://econtract-demo.mobifone.ai/service',
-  // flag: 'NB',
-  flag: 'KD',
+  flag: 'NB',
+  //flag: 'KD',
   SSO_URL: 'https://auth-sso.mobifone.vn:8080/oauth',
   SSO_REALM: 'sso-mobifone',
   SSO_CLIENTID: 'TTCNTT-ECONTRACT-STAGING',
