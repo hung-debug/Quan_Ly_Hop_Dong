7f23fd2d3ca5f358d7a55f03c9af9c26af537f28
Tu-MBF <davidanh2811@gmail.com>
2025-05-22 10:22:00 +0700

    Check flag NB KD

--- src/app/main/digital-certificate/digital-certificate-add/digital-certificate-add.component.ts
+++ src/app/main/digital-certificate/digital-certificate-add/digital-certificate-add.component.ts
@@ -17,6 +17,7 @@ import { DigitalCertificateService } from 'src/app/service/digital-certificate.s
 import { UserService } from 'src/app/service/user.service';
 import { MultiSelectModule } from 'primeng/multiselect';
 import { UnitService } from 'src/app/service/unit.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-user',
@@ -56,7 +57,8 @@ export class DigitalCertificateAddComponent implements OnInit {
   listOrgCombobox: any[];
   selectedNodeOrganization:any = '';
   organization_id:any = "";
-  currentOrgId: any = ""
+  currentOrgId: any = "";
+  environment: any = "";
 
   get f() { return this.addForm.controls; }
   constructor(
@@ -77,6 +79,7 @@ export class DigitalCertificateAddComponent implements OnInit {
       orgId: this.fbd.control("", [Validators.required]),
       status: 1,
     });
+    this.environment = environment
   }
   async ngOnInit(): Promise<void> {
     this.currentOrgId = JSON.parse(localStorage.getItem('currentUser') || '').customer.info.organizationId.toString()
